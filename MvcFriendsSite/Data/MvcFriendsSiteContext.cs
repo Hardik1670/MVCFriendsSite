df5f7b4ada1e7b55650709a5d38951918b388091
Hardik1670 <66189769+Hardik1670@users.noreply.github.com>
2020-11-12 21:32:48 -0500

    MVC Friends Site

---
+++ MvcFriendsSite/Data/MvcFriendsSiteContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcFriendsSite.Models;
+
+namespace MvcFriendsSite.Data
+{
+    public class MvcFriendsSiteContext : DbContext
+    {
+        public MvcFriendsSiteContext (DbContextOptions<MvcFriendsSiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcFriendsSite.Models.UserModel> UserModel { get; set; }
+
+        public DbSet<MvcFriendsSite.Models.SocialModel> SocialModel { get; set; }
+    }
+}
---
+++ MvcFriendsSite/Migrations/20201111032101_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcFriendsSite.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "alias",
+                table: "UserModel",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "socialWebsite",
+                table: "UserModel",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "website",
+                table: "UserModel",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "alias",
+                table: "UserModel");
+
+            migrationBuilder.DropColumn(
+                name: "socialWebsite",
+                table: "UserModel");
+
+            migrationBuilder.DropColumn(
+                name: "website",
+                table: "UserModel");
+        }
+    }
+}
